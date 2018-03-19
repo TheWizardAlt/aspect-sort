f5d6acf7a2f1df5d150eefec3a2bc875bdb901d6
TheWizardAlt <TheWizardAlt@gmail.com>
2018-03-19 17:57:22 -0500

    Working parsecsv

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "sorter.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 vector<string> parseCommas(string setting);
@@ -8,21 +9,44 @@ int main(){
     //TODO: functional main (all code below is temporary/testing)
     sorter *s = new sorter();
 
-    s = new sorter("./", 0, 2, "./sort/");
+    s = new sorter("./", 1, 2, "./sort/");
 
-    cout << s->toString();
     s->findImages();
-    cout << s->toString();
     s->aspectSort();
     cout << s->toString();
 
     vector<string> v = parseCommas(s->toCSV());
+    for(string s : v)
+        cout << s << endl;
+
+    s = new sorter();
+    s->setSearchPath(v[0]);
+
+    stringstream ss(v[1]);
+    double temp;
+    ss >> temp;
+    s->setMinAR(temp);
+
+    temp = 0;
+    ss.clear();
+    ss << v[2];
+    ss >> temp;
+    s->setMaxAR(temp);
+    s->setOutputPath(v[3]);
+
+    s->findImages();
+    s->aspectSort();
+    cout << s->toString();
 
     return 0;
 }
 
 vector<string> parseCommas(string setting){
     vector<string> settings;
-    //string setting1 = setting.find(",");
-    cout << setting.substr(0, setting.find(","));
+    for(int i = 0; i < 4; i++){
+        string s = setting.substr(0,setting.find(","));
+        setting = setting.substr(setting.find(",")+1,setting.length() - 1);
+        settings.push_back(s);
+    }
+    return settings;
 }
